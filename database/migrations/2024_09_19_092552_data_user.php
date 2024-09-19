0db15399901674d469913c392f5be4f20718625f
aedills <zaedilfebriansyah@gmail.com>
2024-09-19 17:30:52 +0800

    Add migration

---
+++ database/migrations/2024_09_19_092552_data_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_user', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('nama', 255);
+            $table->string('username', 25)->unique();
+            $table->string('p4ssw0rd', 255);
+            $table->string('foto', 255)->default('default.png');
+            $table->date('tgl_mulai');
+            $table->date('tgl_selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_user');
+    }
+};
