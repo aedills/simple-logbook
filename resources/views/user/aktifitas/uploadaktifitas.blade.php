b07cc5ae482832157cc337df96e77cbd486363c3
aedills <zaedilfebriansyah@gmail.com>
2024-09-22 22:10:18 +0800

    Modify Upload Aktifitas form

--- resources/views/user/aktifitas/uploadaktifitas.blade.php
+++ resources/views/user/aktifitas/uploadaktifitas.blade.php
@@ -18,44 +18,52 @@
                 <li class="breadcrumb-item active">Upload</li>
             </ol>
         </nav>
-    </div><!-- End Page Title -->
+    </div>
 
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <form enctype="multipart/form-data" action="{{ route('user.aktifitasuser.upload.store') }}" method="POST" class="p-4">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="card-title">Upload Aktifitas/Kegiatan</h5>
+                        </div>
+                        <form id="aktifitasForm" enctype="multipart/form-data" action="{{ route('user.aktifitasuser.upload.store') }}" method="POST">
                             @csrf
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Judul Kegiatan</label>
-                                <input required name="judul" type="text" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp" placeholder="Masukkan judul kegiatan">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="judul">Judul Kegiatan</label>
+                                <div class="col-sm-10">
+                                    <input required name="judul" type="text" class="form-control" id="judul" placeholder="Masukkan judul kegiatan">
+                                </div>
                             </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Deskripsi</label>
-                                <input required name="deskripsi" type="text" class="form-control" id="exampleInputPassword1"
-                                    placeholder="Jelaskan kegiatan kamu">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="deskripsi">Deskripsi</label>
+                                <div class="col-sm-10">
+                                    <textarea required name="deskripsi" id="deskripsi" rows="3" placeholder="Jelaskan kegiatan kamu" class="form-control"></textarea>
+                                </div>
                             </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Gambar</label>
-                                <input required name="gambar" class="form-control" type="file" id="formFile" accept="image/*"
-                                    onchange="previewImage(event)">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="gambar">Gambar</label>
+                                <div class="col-sm-10">
+                                    <input required name="gambar" class="form-control" type="file" id="formFile" accept="image/*" onchange="previewImage(event)">
+                                </div>
                             </div>
-                            <div class="img-container">
-                                <img id="imgPreview" class="img-preview" src="#" alt="Image Preview"
-                                    style="display: none;">
+                            <div class="row mb-3" style="display: none;" id="imgPreviewContainer">
+                                <label class="col-sm-2 col-form-label">Preview</label>
+                                <div class="col-sm-10 img-container">
+                                    <img id="imgPreview" class="img-preview" src="#" alt="Image Preview" style="display: none;">
+                                </div>
                             </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Tanggal</label>
-                                <input required name="tanggal" type="date" class="form-control" id="tanggal">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="tanggal">Tanggal</label>
+                                <div class="col-sm-10">
+                                    <input required name="tanggal" type="date" class="form-control" id="tanggal" value="{{ now()->format('Y-m-d') }}">
+                                </div>
                             </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Upload</button>
                         </form>
+                        <div class="d-flex justify-content-end align-items-end">
+                            <button type="submit" form="aktifitasForm" class="btn btn-primary">Upload</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -67,12 +75,14 @@
     function previewImage(event) {
         const input = event.target;
         const imgPreview = document.getElementById('imgPreview');
+        const imgPreviewContainer = document.getElementById('imgPreviewContainer');
 
         if (input.files && input.files[0]) {
             const reader = new FileReader();
             reader.onload = function(e) {
                 imgPreview.src = e.target.result;
                 imgPreview.style.display = 'block';
+                imgPreviewContainer.removeAttribute('style');
             };
             reader.readAsDataURL(input.files[0]);
         }
